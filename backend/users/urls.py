a238fdc37e91551a80d16dcb6630d4b631845260
Crowdbotics <team@crowdbotics.com>
2020-09-19 23:30:11 +0000

    Update models

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import (
     UserUpdateView,
-    user_detail_view,
     UserDetailView,
     user_update_view,
+    user_detail_view,
     user_redirect_view,
 )
 
